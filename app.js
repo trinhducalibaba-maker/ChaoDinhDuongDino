65b485970eb69c6b517d90e28637e7bace1eff80
Massachusetts Institute of Technology <trinhduc.alibaba@gmail.com>
2026-04-08 17:30:13 +0700

    Update app.js
    Thay đổi WEBHOOK

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ function submitOrder() {
     };
 
     // --- BẠN DÁN LINK WEBHOOK CỦA GOOGLE SHEETS VÀO ĐÂY --- //
-    const WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbxJ0zLtrqMf77FKJY-7i5OuZ1t2VutqRL9TjqJivsYb765tDlKV8i6JcSoRExL3kOPo/exec';
+    const WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbw7y2jyG6xrORFntweIDGHy7eP2hr6hmJw6ur2HueZHEEPXV2F2FJKRRDUIGcxlgcWT/exec';
 
     fetch(WEBHOOK_URL, {
         method: 'POST',
